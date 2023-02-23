8c9b88e67ead8210e94c6a207e355890dd7b08ec
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2023-02-23 23:06:46 +0100

    revert: no longer try to install git if it's not found
    The required packages should be added by something that is more
    sophisticated in doing so, i.e. `common-utils`

--- src/apple-swift-format/install.sh
+++ src/apple-swift-format/install.sh
@@ -2,30 +2,10 @@
 
 set -e
 
-# Clean up
-rm -rf /var/lib/apt/lists/*
-
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
-
-apt_get_update() {
-    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-        echo "Running apt-get update..."
-        apt-get update -y
-    fi
-}
-
-# Checks if packages are installed and installs them if not
-ensure_installed() {
-    if ! dpkg -s "$@" >/dev/null 2>&1; then
-        apt_get_update
-        apt-get -y install --no-install-recommends "$@"
-    fi
-}
-
-export DEBIAN_FRONTEND=noninteractive
 
 if [ "$VERSION" = "latest" ]; then
     LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/apple/swift-format/releases/latest)
@@ -38,8 +18,6 @@ if ! type swift >/dev/null 2>&1; then
     echo "No swift executable found."
     exit 1
 fi
-
-ensure_installed git
 
 echo "Cloning swift-format on $VERSION..."
 
@@ -57,6 +35,5 @@ mv .build/release/swift-format /usr/local/bin/swift-format
 echo "Cleaning up..."
 cd ..
 rm -rf swift-format
-rm -rf /var/lib/apt/lists/*
 
 echo "Done!"
